879ffe8e82effa9a6701ad047011f6f16cdfd806
s-galvez19 <stikgalvez@hotmail.com>
2018-06-28 18:38:04 -0400

    final

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 // no API key required for this API
 const API_URL = 'https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?'
+var button = document.querySelector('.button');
+var quoteA = document.querySelector('.quote-author');
+var quoteT = document.querySelector('.quote-text');
+
+
 
 function getQuote() {
     $.ajax({
         url: API_URL,
-        dataType: 'json'
-    })
-    .done(function (response) {
-        console.log(response);
-
-        
-    })
+        dataType: 'json',
+        success : function(data){
+            console.log(data);
+            quoteA.textContent = data.quoteAuthor;
+            quoteT.textContent = data.quoteText;
+        },
+    
+    error: function(error){
+        console.log("There was an error");
+    }    
+  })
 }
 
-getQuote()
+function clickHandler(e){
+//    if (button.textContent == "Get new Quote"){
+        quoteT.textContent = getQuote();
+//    }
+}
+
+//
+button.addEventListener('click', clickHandler);
+
